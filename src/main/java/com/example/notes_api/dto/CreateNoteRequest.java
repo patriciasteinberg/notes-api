7ec630460bc5299bdf139fab8593b411ca5055b6
patriciasteinberg <patriciacsteinberg@gmail.com>
2026-05-20 14:49:12 -0400

    add controller, repo, service, and dtos

---
+++ src/main/java/com/example/notes_api/dto/CreateNoteRequest.java
@@ -0,0 +1,4 @@
+package com.example.notes_api.dto;
+
+public record CreateNoteRequest (String title, String content) {
+}
--- src/main/java/com/example/notes_api/service/NoteService.java
+++ src/main/java/com/example/notes_api/service/NoteService.java
@@ -1,27 +1,55 @@
 package com.example.notes_api.service;
 
-import com.example.notes_api.model.Note;
+import com.example.notes_api.dto.CreateNoteRequest;
+import com.example.notes_api.dto.NoteResponse;
+import com.example.notes_api.entity.Note;
+import com.example.notes_api.repository.NoteRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class NoteService {
-    private final List<Note> notes = new ArrayList<>();
-    private Long nextId = 1L;
+    private final NoteRepository noteRepository;
 
-    public List<Note> getAllNotes() {
-        return notes;
+    public List<NoteResponse> findAll() {
+        return noteRepository.findAll()
+                .stream()
+                .map(this::toResponse)
+                .toList();
     }
 
-    public Note createNote(String content) {
-        Note note  = new Note(nextId++, content);
-        notes.add(note);
-        return note;
+    public NoteResponse create(CreateNoteRequest request) {
+        Note note = Note.builder()
+                .title(request.title())
+                .content(request.content())
+                .archived(false)
+                .build();
+
+        return toResponse(noteRepository.save(note));
     }
 
-    public void deleteNote(Long id) {
-        notes.removeIf(note -> note.id().equals(id));
+    public void delete(Long id) {
+        noteRepository.deleteById(id);
+    }
+
+    public NoteResponse archive(Long id) {
+        Note note = noteRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException(("Note not found")));
+
+        note.setArchived(true);
+
+        return toResponse(noteRepository.save(note));
+    }
+
+    private NoteResponse toResponse(Note note) {
+        return new NoteResponse(
+                note.getId(),
+                note.getTitle(),
+                note.getContent(),
+                note.isArchived()
+        );
     }
 }
